0dc7fb641c6f9b31c0f6b37a011d30e57dd9adf4
Lee Hayoung <awe080798@gmail.com>
2025-07-23 17:52:18 +0900

    update htmls
    study urls and connect the pages

--- djangogame/djangogame/urls.py
+++ djangogame/djangogame/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+import main.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', main.views.index, name='index'),
+    path('page1/', main.views.page1, name='page1'),
 ]
